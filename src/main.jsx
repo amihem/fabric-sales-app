9350760752223391be39e422dc1b5e48d98ba79e
AMIT KUMAR <amihem.net@gmail.com>
2026-06-15 19:04:42 +0530

    Initial commit

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import SalesApp from './SalesApp.jsx'   // ← Changed
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <SalesApp />
+  </React.StrictMode>,
+)
